beee56a3c621f5f093c80b7c746731c8fe42998d
Erki <luikerki@gmail.com>
2015-05-23 22:30:47 +0300

    Two column structure. Added background.

--- scripts.js
+++ scripts.js
@@ -3,7 +3,8 @@
 var myApp = angular.module("myApp", ['angular.filter']);
 
 myApp.controller('CalendarItemsCtrl', function ($scope, $http) {
-    $scope.CalendarItems = [];
+    $scope.CalendarItemsLeft = [];
+    $scope.CalendarItemsRight = [];
 
     var req = new XMLHttpRequest();
     var key = "AIzaSyDE_JvtqCn80h0oV0TlrXEYaeJbbHLo8Fo";
@@ -12,6 +13,7 @@ myApp.controller('CalendarItemsCtrl', function ($scope, $http) {
     //min end time
     var timeMin = timeNow.toISOString();
     var requrl = "https://www.googleapis.com/calendar/v3/calendars/" + id + "/events?timeMin=" + timeMin + "&key=" + key;
+    var counter = 1;
 
     $http({method: "GET", url: requrl}).success(
             function (data, status, headers, config) {
@@ -20,10 +22,16 @@ myApp.controller('CalendarItemsCtrl', function ($scope, $http) {
                     var time = convertDateToTime(dateTime);
                     
                     // ng seems to sort groups A-Z, need to have date in front of day to have correct sorting.
-                    var date = month[dateTime.getMonth()] + " " + dateTime.getDate() + ", " + weekday[dateTime.getDay()];
+                    var date = weekday[dateTime.getDay()] + ", " + month[dateTime.getMonth()] + " " + dateTime.getDate();
                     var name = item.summary;
                     var event = time + " - " + name;
-                    $scope.CalendarItems.push({date: date, event: event});
+                    if(counter % 2 !== 0) {
+                        $scope.CalendarItemsLeft.push({date: date, event: event});
+                    }
+                    else {
+                        $scope.CalendarItemsRight.push({date: date, event: event});                    
+                    }                 
+                    counter++;
                 });
             });
     var weekday = new Array(7);
@@ -47,7 +55,7 @@ myApp.controller('CalendarItemsCtrl', function ($scope, $http) {
     month[9] = "Oct";
     month[10] = "Nov";
     month[11] = "Dec";
-
+    
     function convertDateToTime(date){
         var minutes = date.getMinutes();
         var hours = date.getHours();
